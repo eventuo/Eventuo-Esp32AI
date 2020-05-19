60af5b7bdabad705dca352da058fdeac0882c8c3
eventuo <eventuoto@proton.me>
2020-05-19 12:52:13 +0000

    Update timer.js

--- components/briand_ai/BriandCNN.cpp
+++ components/briand_ai/BriandCNN.cpp
@@ -1,1 +1,4 @@
-/** Copyright (C) 2023 briand (https://g
+/** Copyright (C) 2023 briand (https://github.com/briand-hub)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Ge
