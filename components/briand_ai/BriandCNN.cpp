cbac771a0c58196f0038733a4b7157ac726232cb
eventuo <eventuoto@proton.me>
2020-05-22 14:34:13 +0000

    safety commit

--- components/briand_ai/BriandCNN.cpp
+++ components/briand_ai/BriandCNN.cpp
@@ -11,4 +11,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.o
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "BriandCNN.hxx"
+
