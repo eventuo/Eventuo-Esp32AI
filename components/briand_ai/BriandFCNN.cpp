5e21313813e396dc884e36c065ee0726f194ee9c
eventuo <eventuoto@proton.me>
2020-05-23 22:34:57 +0000

    Add Travis CI configuration file

--- components/briand_ai/BriandFCNN.cpp
+++ components/briand_ai/BriandFCNN.cpp
@@ -2,4 +2,7 @@
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Fr
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distribute
