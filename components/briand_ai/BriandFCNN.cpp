1e5ae75b14bc62a1e23379acd90afd2247cc0697
eventuo <eventuoto@proton.me>
2020-05-27 12:46:53 +0000

    alex added more bugs

--- components/briand_ai/BriandFCNN.cpp
+++ components/briand_ai/BriandFCNN.cpp
@@ -14,3 +14,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "BriandFCNN.hxx"
+
+using namespace std;
+using namespace Briand;
+
+/***************************************************
