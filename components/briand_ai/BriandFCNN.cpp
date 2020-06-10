b068ab34cdb6244b1b68b35cf782c2472e32d4ea
eventuo <eventuoto@proton.me>
2020-06-10 13:06:16 +0000

    closer

--- components/briand_ai/BriandFCNN.cpp
+++ components/briand_ai/BriandFCNN.cpp
@@ -67,4 +67,6 @@ NeuralLayer::NeuralLayer(const LayerType& type, const size_t& neurons, Activatio
     if (this->_type == LayerType::Input) throw runtime_error("Weights not allowed for input layer.");
 
     // Weight matrix rows must be equal to layer's neurons (not including bias)
-    if (weights.Rows() != neurons) throw runtime_error("Weigh
+    if (weights.Rows() != neurons) throw runtime_error("Weight matrix invalid: must have as many rows as layer's neurons!");
+
+    // Weight matrix cols must be equal to layer's input (cannot chec
