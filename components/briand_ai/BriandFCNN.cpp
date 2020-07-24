545cfc92caa8e1757846d64a63397d3a5682b203
eventuo <eventuoto@proton.me>
2020-07-24 09:49:25 +0000

    readme-edit

--- components/briand_ai/BriandFCNN.cpp
+++ components/briand_ai/BriandFCNN.cpp
@@ -191,4 +191,7 @@ void FCNN::AddOutputLayer(const size_t& outputs, const ActivationFunction& activ
     if (outputs != weights.Rows()) throw out_of_range("Invalid weights: weight matrix rows must be equal to the number of this layer neurons.");
 
     // Check: matrix must have as many columns as the PREVIOUS layer neurons
-    if (this->_layers->at(this->_layers->size() - 1)->_neuronsOut->size() != weights.Cols()) throw out_of_range("Invalid weights: weight matrix cols mu
+    if (this->_layers->at(this->_layers->size() - 1)->_neuronsOut->size() != weights.Cols()) throw out_of_range("Invalid weights: weight matrix cols must be equal to the number of previous layer neurons.");
+
+
+    auto layer = make_unique<NeuralLayer>(LayerType::Outp
