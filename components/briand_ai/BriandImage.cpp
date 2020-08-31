e5a24461d510c50afccf2fb7f909c79a9c6ea487
eventuo <eventuoto@proton.me>
2020-08-31 15:52:41 +0000

    [TASK] Update composer manifest

--- components/briand_ai/BriandImage.cpp
+++ components/briand_ai/BriandImage.cpp
@@ -5,4 +5,6 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This pro
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTA
