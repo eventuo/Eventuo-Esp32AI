c7fe80f911d58bd73804922471627a8efe5bea34
eventuo <eventuoto@proton.me>
2020-10-15 16:21:45 +0000

    Delete config

--- components/briand_ai/BriandMatrix.cpp
+++ components/briand_ai/BriandMatrix.cpp
@@ -92,4 +92,10 @@ void Matrix::Randomize() {
 void Matrix::MultiplyScalar(const double& k) {
     for (size_t i = 0; i < this->_rows; i++) {
         for (size_t j = 0; j < this->_cols; j++) {
-            t
+            this->_matrix[i][j] = this->_matrix[i][j] * k;
+        }
+    }
+}
+
+unique_ptr<Matrix> Matrix::MultiplyMatrix(const Matrix& other) {
+ 
