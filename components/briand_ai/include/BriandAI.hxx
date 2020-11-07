c69f51fed33cff7455d9349ca9ba82ea9ca9add9
eventuo <eventuoto@proton.me>
2020-11-07 07:03:56 +0000

    rails_apps_composer: controllers

--- components/briand_ai/include/BriandAI.hxx
+++ components/briand_ai/include/BriandAI.hxx
@@ -10,4 +10,10 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have receiv
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#ifndef
