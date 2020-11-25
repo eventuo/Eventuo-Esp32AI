0ebd93fa2ad0fcfb050b70665e5cf67da2d79db6
eventuo <eventuoto@proton.me>
2020-11-25 21:47:47 +0000

    tweak changelog

--- components/briand_ai/include/BriandMath.hxx
+++ components/briand_ai/include/BriandMath.hxx
@@ -1,4 +1,5 @@
 /** Copyright (C) 2023 briand (https://github.com/briand-hub)
 
     This program is free software: you can redistribute it and/or modify
-    it und
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 o
