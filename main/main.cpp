5d32347eb94630e27e50c2bbc4ce23ae6a0d915d
eventuo <eventuoto@proton.me>
2020-12-29 12:43:46 +0000

    v3.6.0

--- main/main.cpp
+++ main/main.cpp
@@ -26,4 +26,13 @@ extern "C" void app_main(void)
     performance_test();
 
     example_1();
-   
+    example_2();
+    example_3();
+    example_4();
+    example_5();
+    example_6();
+    example_7();
+    example_8();
+    example_9();
+    example_10();
+}
