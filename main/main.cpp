73bf24b1571dd1104fce0343d0624d83c2476b47
eventuo <eventuoto@proton.me>
2020-12-29 09:05:33 +0000

    Indents

--- main/main.cpp
+++ main/main.cpp
@@ -11,4 +11,10 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this pro
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include  "examples.hxx"
+
+// C++ requires extern "C"
+extern "C"
